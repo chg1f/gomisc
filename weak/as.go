2c59d64ebf3098570cba5d2d3973954b1928e65f
chg1f <chongiofai@gmail.com>
2022-03-03 10:45:08 +0800

    wip
    s

--- weak/as.go
+++ weak/as.go
@@ -6,23 +6,40 @@ import (
 	"strconv"
 )
 
-func AsString(i interface{}) string {
-	if i == nil {
+func AsString(value interface{}) string {
+	if value == nil {
 		return ""
-	} else if x, ok := i.(interface{ String() string }); ok {
+	}
+	v := reflect.ValueOf(value)
+	if v.Kind() == reflect.String {
+		return v.String()
+	} else if v.Type() == reflect.TypeOf([]byte("")) {
+		return string(v.Bytes())
+	} else if x, ok := value.(interface{ String() string }); ok {
 		return x.String()
 	}
-	v := reflect.ValueOf(i)
 	switch v.Kind() {
-	case reflect.String:
-		return v.String()
-	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-		return strconv.FormatInt(v.Int(), 10)
-	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-		return strconv.FormatUint(v.Uint(), 10)
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		return fmt.Sprintf("%d", value)
 	case reflect.Float32, reflect.Float64:
-		return strconv.FormatFloat(v.Float(), 'f', -1, v.Type().Bits())
+		return fmt.Sprintf("%f", value)
 	default:
-		return fmt.Sprintf("%v", i)
+		return fmt.Sprintf("%v", value)
 	}
+}
+
+func AsNumber(value interface{}) float64 {
+	if value == nil {
+		return 0
+	}
+	v := reflect.ValueOf(value)
+	switch v.Kind() {
+	case reflect.Float32, reflect.Float64, reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		return v.Float()
+	}
+	n, err := strconv.ParseFloat(weak.AsString(value), 64)
+	if err != nil {
+		return 0
+	}
+	return n
 }
